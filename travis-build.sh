be05cae47022bfe897049f56ff51a1c7a2007c94
Stefan Biermann <sb@ems-solutions.com>
2017-10-11 09:15:41 +0200

    changing docker version in travis build

--- travis-build.sh
+++ travis-build.sh
@@ -4,9 +4,9 @@ set -e
 docker version
 uname -a
 echo "Updating Docker engine to 17.05.0"
-sudo service docker stop
-curl -fsSL https://get.docker.com/ | sudo sh
-docker version
+#sudo service docker stop
+#curl -fsSL https://get.docker.com/ | sudo sh
+#docker version
 
 if [ "$ARCH" != "amd64" ]; then
   # prepare qemu
